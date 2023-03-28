38ed982479962edd74499f74f0f2de0b70e3457c
ELIJAH <vitoesitemitope@gmail.com>
2023-03-28 19:09:41 +0100

    Update js file

--- modules/Navigation.js
+++ modules/Navigation.js
@@ -1,4 +1,4 @@
-const navigation = () => {
+const Navigation = () => {
   document.querySelector('#book-list-menu').addEventListener('click', () => {
     document.querySelector('.books-container').classList.remove('hide');
     document.querySelector('.form').classList.add('hide');
@@ -18,4 +18,4 @@ const navigation = () => {
   });
 };
 
-export default navigation;
+export default Navigation;

4e5bff685ebcf4a32836847974e469245f946137
ELIJAH <vitoesitemitope@gmail.com>
2023-03-28 19:54:06 +0100

    re-add js file

--- modules/TimeSetup.js
+++
@@ -1,8 +0,0 @@
-import Navigation from './modules/navigation.js';
-import './modules/bookset.js';
-import getTime from './modules/timeSetup.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  Navigation();
-  getTime();
-});
